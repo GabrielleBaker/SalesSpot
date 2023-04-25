5572d2df07d334123f9a35a6a052dd36db4993dc
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2023-04-25 17:06:29 +0300

    security updates

--- Sales_Spot/src/main/java/com/example/Sales_Spot/WebSecurityConfig.java
+++ Sales_Spot/src/main/java/com/example/Sales_Spot/WebSecurityConfig.java
@@ -31,13 +31,20 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests()
-		.requestMatchers("/editTask/**", "/addTask", "/home", "/deletetask/**", "/bestel", "/editbestel/**",
-				"/customerlist", "/newBestel","/newcustomer","/productList","/viewbestel/**").authenticated()
-		.requestMatchers("/signup", "/saveuser").permitAll()
+		.requestMatchers(
+				//home page and tasks
+				"/home","/editTask/**", "/addTask",  "/deletetask/**", 
+				//orders
+				"/bestel", "/editbestel/**", "/newBestel","/viewbestel/**","/deletebestel/**","/savebestel",
+				//customers
+				"/customerlist", "/editcustomer/**","/newcustomer","/deletecustomer/**","/savecustomer",
+				//products
+				"/productList","/editproduct/**","/newproduct","/deleteproduct/**","/saveproduct",
+				//users
+				"/saveuser","/signup"
+				).authenticated()
 		.and()
-		.authorizeHttpRequests().anyRequest().authenticated()
-		//.and()
-		//.headers().frameOptions().disable() //for h2 console		
+		.authorizeHttpRequests().anyRequest().authenticated()		
 		.and()
 		.formLogin()
 		.loginPage("/login")
